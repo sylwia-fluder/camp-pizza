f6fbb3191f0f85e917e881c7b007910c6cab2b75
Asnod <n.dlugaszek@gmail.com>
2019-09-16 10:25:49 +0200

    Added js files

--- root/client/webpack.config.js
+++ root/client/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: '.public/dist'
     },
     plugins: [
         new CleanWebpackPlugin(),
