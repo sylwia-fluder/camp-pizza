82ae744a073621fb8456e572cee1b7ae87828d69
Asnod <n.dlugaszek@gmail.com>
2019-09-17 11:12:52 +0200

    Added routes

--- root/server/.prettierrc.js
+++ root/server/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    bracketSpacing: false
+ trailingComma: "es5",
+ tabWidth: 2,
+ semi: true,
+ singleQuote: true,
+ bracketSpacing: false
 };
